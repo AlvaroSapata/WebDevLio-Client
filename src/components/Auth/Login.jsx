79eff70602a9547ee35f74942b55fd528f2b543d
Jonatan Iglesias <jonathan-18719@outlook.es>
2023-06-14 12:09:07 +0200

    login done

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useContext } from 'react'
+import { loginService } from '../../services/auth.services';
+import {useNavigate} from "react-router-dom"
+import { AuthContext } from '../../context/auth.context';
+function Login() {
+
+    const { authenticateUser } = useContext(AuthContext)
+
+    const navigate = useNavigate()
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+   try {
+    //token con las credenciales
+    const response = await loginService({
+        email: email,
+        password: password
+    })
+
+     //2. guardamos el token en el local storage
+     localStorage.setItem("authToken", response.data.authToken);
+     await authenticateUser()
+     //3. redireccionamos a la pantalla privada para solo usuarios
+     navigate("/");
+   } catch (error) {
+    if (error.response.status === 400) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        navigate("/");
+      }
+   }
+  }
+
+  return (
+    <div>
+     <h2>Login</h2>
+     <form onSubmit={handleLogin}>
+        <div>
+            <label>Email</label>
+            <input type="email" value={email} onChange={handleEmailChange} />
+        </div>
+
+        <div>
+            <label>password</label>
+            <input type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+
+        <br />
+        <button className="myButtons" type="submit" style={{ width: "auto" }}>Log In</button>
+        <br />
+        {errorMessage && <p style={{ color: "#03e9f4" }}>{errorMessage}</p>}
+     </form>
+
+    </div>
+  )
+}
+
+export default Login
